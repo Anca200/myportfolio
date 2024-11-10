2ce91f140f4b9073782f2b682a98b079be41d83e
Anca200 <anca2k17@gmail.com>
2024-11-10 16:12:58 +0100

    update

--- src/app/components/Roadmap.js
+++ src/app/components/Roadmap.js
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
+import LandingPage from "../public/images/LandingPages/LandignPageMain.png";
+import Image from 'next/image';
 
 const Roadmap = () => {
   const router = useRouter();
@@ -13,7 +13,7 @@ const Roadmap = () => {
   }, [router.pathname]);
 
   const projects = [
-    { title: "Landing Pages", href:"/LandingPages"  },
+    { title: "Landing Pages", href: "/LandingPages", image: LandingPage },
     { title: "Coming Soon", href: "/projects/nextjs-website" },
     { title: "Coming Soon", href: "/projects/ecommerce-platform" },
   ];
@@ -22,23 +22,34 @@ const Roadmap = () => {
     <section className="flex flex-col items-center py-10 bg-gradient-to-r from-custom_green_light to-custom_green_dark" id="roadmap">
       <div className="relative w-full lg:w-[80%] flex flex-col items-center">
         
-        {/*Middle Line*/}
+        {/* Middle Line */}
         <div className="absolute left-1/2 transform -translate-x-1/2 w-[2px] bg-[#8dfdcc] h-full hidden lg:block"></div>
 
         {projects.map((project, index) => (
           <div key={index} className="flex flex-col lg:flex-row items-center w-full mb-12">
             <div className="w-full lg:w-1/2 flex justify-center lg:justify-end mb-4 lg:mb-0 pr-0 lg:pr-8">
-              <Link href={project.href} className="text-lg lg:text-[2rem]  text-white text-center lg:text-right transition-all duration-500 transform hover:scale-105 hover:text-shadow-neon font-marcelus font-bold shadow-2xl">
+              <Link href={project.href} className="text-lg lg:text-[2rem] text-white text-center lg:text-right transition-all duration-500 transform hover:scale-105 hover:text-shadow-neon font-marcelus font-bold shadow-2xl">
                 {project.title}
               </Link>
             </div>
 
             <div className="relative flex justify-center w-full lg:w-0 lg:justify-center mb-4 lg:mb-0">
-              <div className="absolute left-1/2 transform -translate-x-1/2 w-6 h-6 bg-[#8dfdcc] rounded-full  shadow-neon"></div>
+              <div className="absolute left-1/2 transform -translate-x-1/2 w-6 h-6 bg-[#8dfdcc] rounded-full shadow-neon"></div>
             </div>
 
             <div className="w-full lg:w-1/2 flex justify-center lg:justify-start pl-0 lg:pl-8">
-              <div className="w-[250px] h-[150px] sm:w-[300px] sm:h-[200px] lg:w-[400px] lg:h-[300px] rounded-md  bg-cover bg-center shadow-neon"></div>
+              <div className="relative w-[250px] h-[150px] sm:w-[300px] sm:h-[200px] lg:w-[400px] lg:h-[300px] rounded-md shadow-neon overflow-hidden">
+                {/* Conditionally render the Image component if the project has an image */}
+                {project.image && (
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-md"
+                  />
+                )}
+              </div>
             </div>
           </div>
         ))}
